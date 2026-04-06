18e48a2352a3c197d567605187cfc7a0ccef6dc4
HANIYA <haniyaelgwizi@gmail.com>
2026-04-06 16:57:23 +0100

    initial commit avec Personne.java

---
+++ Personne.java
@@ -0,0 +1,9 @@
+public class Personne {
+    public static void main(String[] args) {
+        String nom = "Haniya";
+        String prenom = "El Gwizi";
+
+        System.out.println("Nom : " + nom);
+        System.out.println("Prenom : " + prenom);
+    }
+}
