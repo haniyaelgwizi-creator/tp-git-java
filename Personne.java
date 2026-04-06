c46291edfaecd73e6386375dbbb34b66387ea398
HANIYA <haniyaelgwizi@gmail.com>
2026-04-06 17:05:19 +0100

    changer nom en name dans dev

--- Personne.java
+++ Personne.java
@@ -1,9 +1,9 @@
 public class Personne {
     public static void main(String[] args) {
-        String nom = "Haniya";
+        String name = "Haniya";
         String prenom = "El Gwizi";
 
-        System.out.println("Nom : " + nom);
+        System.out.println("Nom : " + name);
         System.out.println("Prenom : " + prenom);
     }
 }
